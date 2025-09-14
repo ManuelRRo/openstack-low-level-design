c723f7dff5fe50bb823ebd42233551f6d8c7fec9
ManuelRRo <ds23906@gmail.com>
2025-09-14 12:23:35 -0600

    extra info osa

--- archivos-adicionales/netplanConfigTest1/network-configuration.sh
+++ archivos-adicionales/netplanConfigTest1/network-configuration.sh
@@ -22,13 +22,39 @@ openstack router set router --external-gateway ext-net
 
 openstack port list --router router
 
+# Create Vlan Network instead of external 
+openstack network create ext-vlan \
+  --external --share \
+  --provider-network-type vlan \
+  --provider-physical-network physnet1 \
+  --provider-segment 40
 
+openstack subnet create ext-vlan-subnet \
+  --network ext-vlan \
+  --subnet-range 172.29.248.0/22 \
+  --gateway 172.29.248.1 \
+  --allocation-pool start=172.29.248.100,end=172.29.251.200 \
+  --dns-nameserver 8.8.4.4 \
+  --no-dhcp
+
+openstack network create selfservice
+openstack subnet create selfservice-subnet \
+  --network selfservice \
+  --subnet-range 172.16.1.0/24 \
+  --gateway 172.16.1.1 \
+  --dns-nameserver 8.8.4.4
+
+openstack router create router
+openstack router add subnet router selfservice-subnet
+openstack router set router --external-gateway ext-vlan
+openstack port list --router router
+
+#################################
 openstack flavor create --id 0 --vcpus 1 --ram 64 --disk 1 m1.nano
 openstack flavor create --id 0 --vcpus 1 --ram 512 --disk 20 m1.medium
 
 
 openstack keypair create --public-key id_rsa.pub mykey
-
 
 openstack security group rule create --proto icmp default
 
@@ -36,16 +62,21 @@ openstack security group rule create --proto tcp --dst-port 22 default
 
 
 wget http://download.cirros-cloud.net/0.4.0/cirros-0.4.0-x86_64-disk.img
+wget https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.img
 
-glance image-create --name "cirros" \
-  --file cirros-0.4.0-x86_64-disk.img \
+glance image-create --name "ubuntu-noble" \
+  --file noble-server-cloudimg-amd64.img \
   --disk-format qcow2 --container-format bare \
+  --property os_type=linux \
   --visibility=public
 
-openstack server create --flavor m1.nano --image cirros \
-  --nic net-id=0622173a-a331-4eba-bb70-5d0e1aaeb40d --security-group default \
-  --key-name mykey selfservice-instance
+openstack server create --flavor m1.medium --image ubuntu-noble \
+  --nic net-id=4ae01051-271c-4a9c-a0a4-1f69cf3bc7b5 --security-group default \
+  --key-name mykey ubuntu
 
-openstack floating ip create ext-net
+openstack floating ip create ext-vlan
 
 openstack server add floating ip selfservice-instance 203.0.113.104
+
+
+
