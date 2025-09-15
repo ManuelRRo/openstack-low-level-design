1f14cd984b826b113d129132b9e364f95799022a
ManuelRRo <ds23906@gmail.com>
2025-09-14 23:07:13 -0600

    heat template v1

--- archivos-adicionales/netplanConfigTest1/network-configuration.sh
+++ archivos-adicionales/netplanConfigTest1/network-configuration.sh
@@ -38,6 +38,7 @@ openstack subnet create ext-vlan-subnet \
   --no-dhcp
 
 openstack network create selfservice
+
 openstack subnet create selfservice-subnet \
   --network selfservice \
   --subnet-range 172.16.1.0/24 \
@@ -51,7 +52,8 @@ openstack port list --router router
 
 #################################
 openstack flavor create --id 0 --vcpus 1 --ram 64 --disk 1 m1.nano
-openstack flavor create --id 0 --vcpus 1 --ram 512 --disk 20 m1.medium
+openstack flavor create --id 1 --vcpus 1 --ram 512 --disk 20 m1.medium
+openstack flavor create --id 2 --vcpus 1 --ram 1024 --disk 20 m1.large
 
 
 openstack keypair create --public-key id_rsa.pub mykey
@@ -61,7 +63,7 @@ openstack security group rule create --proto icmp default
 openstack security group rule create --proto tcp --dst-port 22 default
 
 
-wget http://download.cirros-cloud.net/0.4.0/cirros-0.4.0-x86_64-disk.img
+wget https://github.com/cirros-dev/cirros/releases/download/0.6.3/cirros-0.6.3-aarch64-disk.img
 wget https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.img
 
 glance image-create --name "ubuntu-noble" \
@@ -70,8 +72,14 @@ glance image-create --name "ubuntu-noble" \
   --property os_type=linux \
   --visibility=public
 
+glance image-create --name "cirros" \
+  --file cirros-0.6.3-aarch64-disk.img \
+  --disk-format qcow2 --container-format bare \
+  --property os_type=linux \
+  --visibility=public
+
 openstack server create --flavor m1.medium --image ubuntu-noble \
-  --nic net-id=4ae01051-271c-4a9c-a0a4-1f69cf3bc7b5 --security-group default \
+  --nic net-id=4cb1c162-5ac0-4a6f-a309-6e26767ac3b5 --security-group default \
   --key-name mykey ubuntu
 
 openstack floating ip create ext-vlan
